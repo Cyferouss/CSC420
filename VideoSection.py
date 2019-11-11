db08c4f1885c4f9326fbc0a7d071e46d493a3a23
Adrian <blizzardsnows@gmail.com>
2019-11-10 20:51:31 -0500

    .

--- VideoSection.py
+++ VideoSection.py
@@ -13,8 +13,23 @@ for i in getImageFromVideo("C:/Users/skunk/Downloads/FMSD26.mov"):
     cv2.waitKey(0)
 '''
 
-def placeImage(x, y, big, nose):
-    pass
+# gray scale?
+def placeImage(coords, big, nose):
+    # Need to  make sure it doesn't go out of bounds.
+    x,y = coords
+    img3 = big.copy()
+
+    img3[x:x+nose.shape[0], y:y+nose.shape[1], :] = nose[:,:,:]
+    cv2.imshow("", img3)
+
+# Test placeImage
+img1 = cv2.imread("C:/Users/skunk/Desktop/49897907_271940710169544_307813064789458944_n.png")
+img2 = cv2.imread("C:/Users/skunk/Desktop/images.png")
+
+for i in range(0, 500):
+    placeImage((i, i), img1, img2)
+    cv2.waitKey(delay=20)
+
 
 # General Flow
 '''
@@ -27,7 +42,4 @@ for i in getImageFromVideo("C:/Users/skunk/Downloads/FMSD26.mov"):
     
     # Write frame
     # out.write(frame)
-
-
-
 '''
