c8ef6948a7edad39ea067b7fc7328a0dcd04873c
blizzardsnows@gmail.com <blizzardsnows@gmail.com>
2019-11-29 23:01:42 -0500

    poooop

--- VideoSection.py
+++ VideoSection.py
@@ -9,10 +9,12 @@ def getImageFromVideo(path):
         success, image = vidcap.read()
 
 # Gray scale?
-def placeImage(coords, scene, img, scale=.5, debug=True):
+def placeImage(coords, scene, img, scale=.1, debug=True):
     # Need to  make sure it doesn't go out of bounds.
     x1, y1 = coords
     img3 = scene.copy()
+
+    img = cv2.resize(img, (int(img.shape[0]*scale), int(img.shape[1]*scale)))
     xCenter = int(img.shape[1]/2)
     yCenter = int(img.shape[0]/2)
     for i in range(-xCenter, xCenter):
@@ -28,9 +30,11 @@ def placeImage(coords, scene, img, scale=.5, debug=True):
                 pass
     return img3
 
-def augmentFlowers(scene, numFlowers=20, minDistance=300, debug=False):
+def augmentFlowers(scene, numFlowers=20, minDistance=50, debug=False):
     orb = cv2.ORB_create()
     kp1, desc1 = orb.detectAndCompute(scene, None)
+    if (len(kp1) == 0):
+        return scene
 
     if debug:
         img2 = cv2.drawKeypoints(scene, kp1, None, color=(255,0,0))
@@ -66,16 +70,37 @@ def augmentFlowers(scene, numFlowers=20, minDistance=300, debug=False):
 # Test placeImage
 img1 = cv2.imread("C:/Users/skunk/Desktop/49897907_271940710169544_307813064789458944_n.png")
 img2 = cv2.imread("C:/Users/skunk/Desktop/images.png")
-flower = cv2.imread("C:/Users/skunk/Desktop/csc420proj/CSC420/flower.png")
+flower = cv2.imread("C:/Users/skunk/Desktop/csc420proj/CSC420/bee2.png")
 
 #augmentedScene = augmentFlowers(img1)
 #cv2.imshow("", augmentedScene)
 # General Flow
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter("C:/Users/skunk/Desktop/csc420proj/CSC420/output.avi", fourcc, 20, (640, 480))
-for i in getImageFromVideo("C:/Users/skunk/Downloads/FMSD26.mov"):
-    #cv2.imshow("", i)
-    #cv2.waitKey(delay=20)
-    out.write(i)
-out.release()
+testing = False
+fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+out = cv2.VideoWriter("C:/Users/skunk/Desktop/csc420proj/CSC420/output.avi", fourcc, 30, (640, 360), True)
 
+if testing:
+    for frame in getImageFromVideo("C:/Users/skunk/Desktop/csc420proj/CSC420/TestVideo.mp4"):
+        augmentedScene = augmentFlowers(frame)
+        out.write(augmentedScene)
+    out.release()
+
+face_cascade = cv2.CascadeClassifier("C:\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_default.xml")
+count = 0
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = cap.read()
+
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame_gray = cv2.equalizeHist(frame_gray)
+    print(frame.shape)
+    #-- Detect faces
+    faces = face_cascade.detectMultiScale(frame_gray)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w,y+h),(255,0,0), 3)
+    cv2.imshow('frame',frame)
+    cv2.waitKey(delay=60)
+
+out.release()
