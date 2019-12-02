144e6034fabcb90f4baff76706adbfc022c2b88b
Cyferouss <tony@clinky.io>
2019-12-02 15:25:03 -0500

    Adding trained neural net, resized squid nose picture, drawing nose algorithm, and pre-trained weights

--- SquidVision_NN.py
+++ SquidVision_NN.py
@@ -55,29 +55,30 @@ def load_dataset(set_size=16, folder_path='test_images/', extension_name='.png')
     return np.array(image_set)
 
 def sourced_cnn():
+    #sourced from paper that we referred to
     model = Sequential()
     
     model.add(Convolution2D(64, (3,3), padding='valid', use_bias=False, input_shape=(96,96,1)))
-    model.add(LeakyReLU(alpha = 0.25))
+    model.add(LeakyReLU(alpha = 0.2))
     model.add(BatchNormalization())
     
     model.add(MaxPool2D(pool_size=(2, 2)))
 
     model.add(Convolution2D(128, (2,2), padding='valid', use_bias=False))
-    model.add(LeakyReLU(alpha = 0.25))
+    model.add(LeakyReLU(alpha = 0.2))
     model.add(BatchNormalization())
     
     model.add(MaxPool2D(pool_size=(2, 2)))
     
     model.add(Convolution2D(256, (2,2), padding='valid', use_bias=False))
-    model.add(LeakyReLU(alpha = 0.25))
+    model.add(LeakyReLU(alpha = 0.2))
     model.add(BatchNormalization())
     
     model.add(MaxPool2D(pool_size=(2, 2)))
      
     model.add(Flatten())
     model.add(Dense(500,activation='relu', kernel_regularizer=regularizers.l2(0.003)))
-    model.add(Dense(500,activation='relu', kernel_regularizer=regularizers.l2(0.003)))
+    model.add(Dense(500,activation='relu', kernel_regularizer=regularizers.l2(0.001)))
 
     model.add(Dense(8, activation='sigmoid'))
     
@@ -176,7 +177,7 @@ def homographical_augment(image_set, truth_set):
                     lnms_matrix = local_non_max_suppression(matrix_slice)
                     augmented_t_matrix[p-2:p+2, k-2:k+2] = lnms_matrix
                     
-        #Create Truths
+        #Create Truths for Homography
         augmented_x = []
         augmented_y = []
         for p in range(96):
@@ -189,6 +190,8 @@ def homographical_augment(image_set, truth_set):
         if(len(augmented_x) == 4):
             augmented_coordinates.append((augmented_x, augmented_y))
             augmented_images.append(np.expand_dims(augmented_image, axis=-1))
+
+        #Flip augmentation
 
         flip = random.choice([0,1,-1])
         flipped_image = cv2.flip(reshaped_img, flip)
@@ -205,14 +208,10 @@ def homographical_augment(image_set, truth_set):
         if(len(flipped_x) == 4):
             flipped_coordinates.append((flipped_x, flipped_y))
             flipped_images.append(np.expand_dims(flipped_image, axis=-1))
-    #Repack coordinates into truth set
+            
+#    Repack coordinates into truth set
     augmented_truths = repack_truths(augmented_coordinates)
     flipped_truths = repack_truths(flipped_coordinates)
-    
-    print(np.shape(augmented_images))
-    print(np.shape(augmented_truths))
-    print(np.shape(flipped_images))
-    print(np.shape(flipped_truths))
     
     augmented_img_set = np.concatenate((augmented_images, flipped_images), axis=0)
     augmented_truth_set = np.concatenate((augmented_truths, flipped_truths), axis=0)
@@ -254,13 +253,7 @@ def repack_truths(coordinates_set):
         
     return truth_set
 
-if __name__ == "__main__":
-#    Built with the help of https://www.kaggle.com/soumikrakshit/eye-detection-using-facial-landmks
-#    Load Dataset and CSV
-    face_images = np.moveaxis(np.load('face_images.npz')['face_images'],-1,0)
-    d_pts = pd.read_csv('facial_keypoints.csv')
-    
-    #Get non-zero datapoints
+def process_dataset(face_images, d_pts):
     non_zero_selection = np.nonzero(d_pts.left_eye_center_x.notna() & 
                            d_pts.right_eye_center_x.notna() & 
                            d_pts.nose_tip_x.notna() & 
@@ -291,33 +284,168 @@ if __name__ == "__main__":
         truth_set[true_index][7] = (d_pts.mouth_center_bottom_lip_y[index]) / image_size
         
         true_index += 1
-            
-    #split between training set and testing set
-    a_t, a_tr = homographical_augment(data_set, truth_set)
-    print("a_t: " + str(np.shape(a_t)))
-    print("a_tr: " + str(np.shape(a_tr)))
-    x_set = np.concatenate((data_set, a_t), axis=0)
-    y_set = np.concatenate((truth_set, a_tr), axis=0)
-    
-    x_train, x_test, y_train, y_test = train_test_split(x_set, y_set, test_size=0.2, random_state=10)
+        
+    return data_set, truth_set
 
-    print("x_train: " + str(np.shape(x_train)))
-    print("y_train: " + str(np.shape(y_train)))
-    testing_data = load_dataset()
+def upscale_coordinates(coordinate_set, starting_size, upscaled_size):
+    coordinates = []
+    x_factor = upscaled_size[0] / starting_size[0]
+    y_factor = upscaled_size[1] / starting_size[1]
     
-    plot_results(x_train, y_train)
+    for i in range(len(coordinate_set)):
+        if i % 2 == 0:
+            coordinates.append(coordinate_set[i] * x_factor)
+        else:
+            coordinates.append(coordinate_set[i] * y_factor)
+        
+    return coordinates
+
+def downscale_coordinates(coordinate_set, starting_size, downscaled_size):
+    coordinates = []
+    x_factor = downscaled_size[0] / starting_size[0]
+    y_factor = downscaled_size[1] / starting_size[1]
+    
+    for i in range(len(coordinate_set)):
+        if i % 2 == 0:
+            coordinates.append(coordinate_set[i] * x_factor)
+        else:
+            coordinates.append(coordinate_set[i] * y_factor)
+        
+    return coordinates
+
+def draw_nose(image, coordinate_set, rgb=False):
+    raw_nose = cv2.imread('SquidNose.png')
+    gray_nose = cv2.cvtColor(raw_nose, cv2.COLOR_BGR2GRAY)
+    
+    #nose_pts = np.array(np.float32([[0,0], [568,0], [287,313], [275,681]]))
+    nose_pts = np.array(np.float32([[0,0], [568,0], [0,681], [568,681]]))
+
+    #Coordinates[0] = Left Eye
+    #Coordinates[1] = Right Eye
+    #Coordinates[2] = Nose
+    #Coordinates[3] = Mouth
+    
+    coordinates = []
+    coordinate = []
+    for i in range(len(coordinate_set)):
+        if i % 2 == 0:
+            coordinate.append(coordinate_set[i] * 96)
+        else:
+            coordinate.append(coordinate_set[i] * 96)
+            coordinates.append(coordinate.copy())
+            coordinate = []
+
+    coordinates = np.array(np.float32(coordinates))
+        #Tl, TR, BL, BR
+    nose_box_lx = coordinates[0][0]
+    nose_box_rx = coordinates[1][0]
+    nose_box_ty = coordinates[0][1]
+    nose_box_by = coordinates[3][1]
+    
+    #Rectangle BB around Nose
+    
+    bb_coordinates = np.array([
+            [nose_box_lx, nose_box_ty],
+            [nose_box_rx, nose_box_ty],
+            [nose_box_lx, nose_box_by],
+            [nose_box_rx, nose_box_by]
+            ])
+    
+    print(coordinates)
+    print(bb_coordinates)
+    print(nose_pts)
+    
+    h, res = cv2.findHomography(nose_pts, bb_coordinates)
+    print(h)
+    
+    if rgb == False:
+        warped_nose = cv2.warpPerspective(gray_nose, h, (np.size(image,0), np.size(image,1)))
+        print(np.shape(image))
+        print(np.shape(warped_nose))
+        augmented_image = image.copy()
+        
+        for y in range(np.size(image, 0) - 15):
+            for x in range(np.size(image,1) - 15):
+                nose_pixel = warped_nose[y][x]
+                if nose_pixel != 0 and nose_pixel != 255:
+                    augmented_image[y][x] = nose_pixel
+    else:
+        nose_b, nose_g, nose_r = cv2.split(raw_nose)
+        img_b, img_g, img_r = cv2.split(image)
+        
+        warped_nose_b = cv2.warpPerspective(nose_b, h, (np.size(image,0), np.size(image,1)))
+        warped_nose_g = cv2.warpPerspective(nose_g, h, (np.size(image,0), np.size(image,1)))
+        warped_nose_r = cv2.warpPerspective(nose_r, h, (np.size(image,0), np.size(image,1)))
+        
+        print(np.shape(warped_nose_b))
+        print(np.shape(warped_nose_g))
+        print(np.shape(warped_nose_r))
+        
+        print(np.shape(img_b))
+        print(np.shape(img_g))
+        print(np.shape(img_r))
+        for y in range(np.size(image, 0)):
+            for x in range(np.size(image,1)):
+                nose_pixel_b = warped_nose_b[y][x]
+                nose_pixel_g = warped_nose_g[y][x]
+                nose_pixel_r = warped_nose_r[y][x]
+                if nose_pixel_b > 20 and nose_pixel_b < 245:
+                    img_b[y][x] = nose_pixel_b
+                    img_g[y][x] = nose_pixel_g
+                    img_r[y][x] = nose_pixel_r
+                    
+        augmented_image = cv2.merge((img_b, img_g, img_r))
+        
+    return augmented_image
+if __name__ == "__main__":
+#    Built with the help of https://www.kaggle.com/soumikrakshit/eye-detection-using-facial-landmks
+#    Load Dataset and CSV
+#    face_images = np.moveaxis(np.load('face_images.npz')['face_images'],-1,0)
+#    d_pts = pd.read_csv('facial_keypoints.csv')
+#    
+#    #Process Data_set
+#    data_set, truth_set = process_dataset(face_images, d_pts)
+#    
+#    #Get augmented data
+#    a_t, a_tr = homographical_augment(data_set, truth_set)
+#
+#    #Append augmented data
+#    x_set = np.concatenate((data_set, a_t), axis=0)
+#    y_set = np.concatenate((truth_set, a_tr), axis=0)
+#    
+#    #Split into training and test sets
+#    x_train, x_test, y_train, y_test = train_test_split(x_set, y_set, test_size=0.2, random_state=10)
+#    
+#    plot_results(x_train, y_train)
     
     #split between training set and validation set    
-    #model = basic_cnn(image_size)
-    model = sourced_cnn()
-    #model.load_weights('ghetto_net_weights.h5')
-    checkpoint = ModelCheckpoint('ghetto_net_weightsv2.h5', monitor='val_accuracy', save_best_only=True, save_weights_only=True, verbose=1)
-    model.fit(x_train, y_train, batch_size=16, epochs=100, callbacks=[checkpoint], validation_split=0.14628, verbose=1, shuffle=True)
+#    model = sourced_cnn()
+#    model.load_weights('ghetto_net_weights_use.h5')
+#    checkpoint = ModelCheckpoint('ghetto_net_weightsv4.h5', monitor='val_accuracy', save_best_only=True, save_weights_only=True, verbose=1)
+#    model.fit(x_train, y_train, batch_size=16, epochs=100, callbacks=[checkpoint], validation_split=0.17, verbose=1, shuffle=True)
+     
+#    predictions = model.predict(x_test)
     
-    predictions = model.predict(x_test)
+    #Real portrait testing
+    testing_data = load_dataset()
     predicted_testing = model.predict(testing_data)
-    plot_results(x_test, predictions)
+    #plot_results(x_test, predictions)
     
+    
+    raw_image = cv2.imread('test_images/15.png')
+    gray_image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2GRAY)
+    
+    print(predicted_testing[7])
+    img_upscaled_dpts = upscale_coordinates(predicted_testing[15], (96,96), (np.size(raw_image,0),np.size(raw_image,1)))
+    print(predicted_testing[7])
+    print(img_upscaled_dpts)
+    
+#    image = np.reshape(x_test[1762], (96,96))
+#    coordinates = predictions[1762]
+    nose = draw_nose(raw_image, img_upscaled_dpts, rgb=True)
+    
+    nose_rgb = cv2.cvtColor(nose, cv2.COLOR_BGR2RGB)
+    mpl.pyplot.imshow(nose_rgb)
     
         
     
